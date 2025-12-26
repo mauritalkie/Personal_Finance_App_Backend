f0de847f2c1ba846f21a1a9ba305ed69124458d2
mauritalkie <mauriciokun13@gmail.com>
2025-12-25 20:56:58 -0600

    enabling cors

--- app/main.py
+++ app/main.py
@@ -2,10 +2,23 @@ from fastapi import FastAPI, Depends
 from api.router import api_router
 from typing import Annotated
 from dependencies import oauth2_scheme
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(api_router)
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 async def root(token: Annotated[str, Depends(oauth2_scheme)]):
